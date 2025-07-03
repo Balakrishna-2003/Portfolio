ca6d56093753208b358a6436922b31cb3b03f1b9
balakrishna <kulalbalakrishna4@gmail.com>
2025-07-03 20:05:46 +0530

    minor changes updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,18 +9,18 @@ function Navbar() {
         <>
             <nav>
                 <ul className='sidebar'>
-                    <li onClick={showSideBar}><a href='#'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
+                    <li onClick={showSideBar}><a href='#'><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
                     <li onClick={showSideBar}><a href='#Home'>Home</a></li>
                     <li onClick={showSideBar}><a href='#Skills'>Skills</a></li>
                     <li onClick={showSideBar}><a href='#Projects'>Projects</a></li>
                     <li onClick={showSideBar}><a href="#Contact">Contact</a></li>
                 </ul>
-                <ul>
+                <ul className='mainnav'>
                     <li className='hideOnMobile'><a href='#Home'>Home</a></li>
                     <li className='hideOnMobile'><a href='#Skills'>Skills</a></li>
                     <li className='hideOnMobile'><a href='#Projects'>Projects</a></li>
                     <li className='hideOnMobile'><a href="#Contact">Contact</a></li>
-                    <li className='menu-btn' onClick={showSideBar}><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
+                    <li className='menu-btn' onClick={showSideBar}><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
                 </ul>
             </nav>
         </>
