2f35d586f6c6ca6e8add76a66190bffc9a985711
balakrishna <kulalbalakrishna4@gmail.com>
2025-07-03 19:11:47 +0530

    updated all images occurings

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,12 +2,11 @@ import react from 'react';
 
 function Skills() {
     const addSkills = () => {
-        const skillsicon = ["react", "android_icon", "express_icon.png", "firebase_icon.png"]
         const skillsElement = ["React","Android","Firebase","Python", "Java", "Javascript", "Linux", "Mongodb","C++","Git", "Nodejs",];
         return skillsElement.map((skills, index) => (
             <div className="card" key={index}>
                 <div className="image_container">
-                    <img className='image' src={"./src/assets/BulkResizePhotos.com/"+skillsElement[index]+".png"} width={"55px"} height={"50px"} />
+                    <img className='image' src={"/assets/allskills/"+skillsElement[index]+".png"} width={"55px"} height={"50px"} />
                     {/* <img className='image' src="https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_logo.svg/1200px-Java_logo.svg.png" alt="Python logo" width="50"/> */}
                 </div>
                 <div className="title">
