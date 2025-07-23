fdfe137c19822421c0be87d5c08d36cbb995438c
balakrishna <kulalbalakrishna4@gmail.com>
2025-07-23 08:09:49 +0530

    used typed.js

--- src/components/Heropage.jsx
+++ src/components/Heropage.jsx
@@ -1,23 +1,39 @@
-import react from 'react';
+import react, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
 
 export default function Heropage() {
+    const el  = useRef(null);
+    useEffect(() => {
+        // Typing animation script start
+       const typing = new Typed(el.current, {
+            strings: ["Hello world","React dev"],
+            typeSpeed: 100,
+            backSpeed: 40,
+            loop: true,
+        });
+        // Typing animation script End
+        return () => {
+            typing.destroy();
+        }
+    }, [])
     return (
         <>
-        <section id='Home'>
-            <div className="aboutmepage">
-                <div className="mynamepart">
-                    <div className="myName">
-                        <div className="mypart">
-                            <div className="backcircle">
-                                <h1 className='piedra-regular'>H</h1>
-                            </div>
+            <section id='Home'>
+                <div className="aboutmepage">
+                    <div className="mynamepart">
+                        <div className="myName">
+                            <div className="mypart">
+                                <div className="backcircle">
+                                    <h1 className='piedra-regular'>H</h1>
+                                </div>
                                 <h1 className='piedra-regular' id='marleft'>ello! I Am</h1>
-                            <h1 className='piedra-regular' style={{color: '#763cac'}}>Balakrishna</h1>
-                        </div>
+                                <h1 className='piedra-regular' style={{ color: '#763cac' }}>Balakrishna</h1>
+                            </div>
                         </div>
 
                         <div className="myrole">
-                            <h2>I'm a software Engineer. <br /> Linked In<br /></h2>
+                            <h2><span ref={el} style={{color: "#763cac"}}></span></h2>
+                            <h3> Linked In</h3>
                         </div>
 
                         <div className="mydesc">
@@ -27,16 +43,16 @@ export default function Heropage() {
                                 life with intuitive designs and clean code. Let's create something
                                 amazing together!.</h3><br />
                         </div>
-                    
-                </div>
-                <div className="myimg">
-                    <div className="background_img">
-                        
+
                     </div>
-                    {/* <img src="./src/assets/21004063.jpg" alt="my image" /> */}
+                    <div className="myimg">
+                        <div className="background_img">
+
+                        </div>
+                        {/* <img src="./src/assets/21004063.jpg" alt="my image" /> */}
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
         </>
     )
 }
